3ee9344ca3d27ce93cfeaa785850d8019cf5548e
Mathew <mathewkariuki2005@gmail.com>
2025-07-18 13:29:54 +0300

    merge

---
+++ Project-Phase2/src/d.jsx
@@ -0,0 +1,7 @@
+function  F(){
+    return(
+        <h1>Hi</h1>
+    )
+}
+
+export default F;
--- Project-Phase2/src/main.jsx
+++ Project-Phase2/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import F from './d.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <F />
   </StrictMode>,
 )
