6d1373de86ecae4fc39afe06bc7580b8a9178eb3
Adrian <markoti2005@gmail.com>
2025-07-19 16:25:49 +0300

    create/style the tiles(not arranged yet) with css in the css file

--- Project-Phase2/src/componets/gameboard.jsx
+++ Project-Phase2/src/componets/gameboard.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./board.css"
 
 function GameBoard() {
     return (
-        <div className="board-container container mt-4">
+        <div className="board-container container mt-8">
             <div className="board-grid">
                 {}
                  {[...Array(40)].map((_, i) => ( // creates an array of undefined values
